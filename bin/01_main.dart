8e9f7c412fd1cb1a66501cd2cc44286aa2eac53d
sagart <sagart@gmail.com>
2024-12-03 15:43:14 +0530

    first commit

---
+++ bin/01_main.dart
@@ -0,0 +1,31 @@
+/* Introduction to Dart 
+- Dart is a client-optimized programming language for apps on multiple platform.
+It is developed by Google and is used to build mobile, desktop, server, and web applications. Dart is an object-oriented, class-based, garbage-collected language with
+C-style syntax. Dart can compile to either native code or Javascript.
+*/
+
+void main() {
+                      /*entry point of all dart programs - main function
+                      void - returns nothing
+                      only code written in this main will execute, no code outside will be executed*/
+
+  // print("Hello Dart");     
+
+  // # Variable Name and Rules
+  /*   
+  A variable is an identifier or a name which is used to refer a value
+  and this value change during program execution.
+
+  */
+                     
+
+
+
+
+
+
+
+
+
+
+}
